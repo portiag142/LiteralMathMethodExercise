66f24d0a344ae5935ef0f593fe7de886bb64d942
Portia Greene <pd12300@yahoo.com>
2021-09-22 23:51:14 -0400

    LiteralMathmethodexercise

---
+++ script.js
@@ -0,0 +1,25 @@
+let warmHugs = "Hi, Im Olaf and I like warm hugs";
+console.log(warmHugs);
+
+console.log(warmHugs.toUpperCase());
+
+console.log(warmHugs.replace("like", "love"));
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+console.log(beenImpaled.slice(18));
+
+let dotdotdot = "...";
+
+let skullBones = `I dont have a skull ${dotdotdot} or bones.`;
+
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNum = Math.random();
+console.log(randomNum);
+randomNum *=3;
+randomNum = Math.floor(randomNum);
+randomNum++;
+console.log(randomNum);
